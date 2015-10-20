b640c9384b568f9366bfe04ea5a386cddca4f510
Greg Kennedy <gkennedy1@gmail.com>
2015-10-20 13:46:24 +1030

    Initial commit of nopCommerce 3.60

---
+++ Presentation/Nop.Web/Plugins/SevenSpikes.Nop.Plugins.StoreLocator/Scripts/StoreLocatorAllShops.js
@@ -0,0 +1,453 @@
+﻿var mapWrapperId = 'all-shops-map-holder';
+var shopCoordinatesElementClass = '.shop-coordinates';
+var allShops = [];
+
+var directionsService;
+var directionsDisplay;
+
+var map;
+var markers = [];
+var userMarker;
+var bounds;
+
+var searchResultCoords;
+
+var shopX = 0;
+var shopY = 0;
+var patternCoords = new RegExp(/^-?\d+\.?\d*$/);
+var isUserMarkerCreated = false;
+
+$(document).ready(function () {
+    if ($(shopCoordinatesElementClass).length > 0 && document.getElementById(mapWrapperId) != null) {
+
+        $(shopCoordinatesElementClass).each(function (index) {
+            var lat = $(this).attr('data-latitude');
+            var lng = $(this).attr('data-longitude');
+
+            var shop = {
+                index: index,
+                lat: lat,
+                lng: lng,
+                title: $(this).attr('data-shop-title')
+            };
+
+            allShops.push(shop);
+        });
+
+        loadMapScript();
+
+        $('#backToResults').on('click', function () {
+            directionsDisplay.setMap(null);
+            $('#directions-panel, .map-wrapper, #backToResults').removeClass('directions-shown');
+        });
+        
+        $('#searchForFilteredShops').on('click', function (e) {
+            e.preventDefault();
+
+            searchForShopsMatchingTheSearchCriteria();
+
+            $('#clearShopFilters').show();
+        });
+
+        $('.shops-list').on('click', '.show-directions', function (e) {
+            e.preventDefault();
+
+            if (!isUserMarkerCreated) {
+                return;
+            }
+
+            var ind = parseInt($(this).parents('li.shops-item').attr('data-ind'));
+            ind >= 0 && showDirectionsToShop(ind);
+        });
+
+        $('.shops-list').on('mouseenter', 'li', function () {
+            var currentMarker = markers[$(this).attr('data-ind')];
+            if (currentMarker != null) {
+                currentMarker.setAnimation(google.maps.Animation.BOUNCE);
+            }
+        }).on('mouseleave', 'li', function () {
+            var currentMarker = markers[$(this).attr('data-ind')];
+            if (currentMarker != null) {
+                currentMarker.setAnimation(null);
+            }
+        });
+
+        $('#clearShopFilters').on('click', function () {
+            showMarkers();
+            $('.shops-list > li').show();
+            $('#searchByTagsInput').val('');
+            $('#searchRadius').val('');
+            $(this).hide();
+        });
+
+        $('#sortingSelect').on('change', sortShopsBySelectedMethod);
+
+        $('.align-map-button').on('click', function() {
+            map.fitBounds(bounds);
+        });
+    }
+});
+
+function loadMapScript() {
+    var script = document.createElement("script");
+    script.src = "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places&sensor=false&callback=initializeMap";
+    document.body.appendChild(script);
+}
+
+function initializeMap() {
+    var map_canvas = document.getElementById(mapWrapperId);
+    var map_options = {
+        center: new google.maps.LatLng(shopX, shopY),
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map(map_canvas, map_options);
+
+    google.maps.event.addListenerOnce(map, 'idle', function () {
+        onMapLoad();
+        createMapSearchBox();
+    });
+}
+
+function onMapLoad() {
+    bounds = new google.maps.LatLngBounds();
+
+    for (var a = 0; a < allShops.length; a++) {
+        addMarker(allShops[a], false);
+    }
+
+    if (allShops.length > 1) {
+        map.fitBounds(bounds);
+    }
+
+    directionsService = new google.maps.DirectionsService();
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    directionsDisplay.setPanel(document.getElementById('directions-panel'));
+
+    $('.getUserGeoLocation').on('click', function (e) {
+        e.preventDefault();
+
+        getUserGeoLocation();
+    });
+}
+
+function addMarker(shopInfo, isDraggable) {
+
+    if (shopInfo.lat == null || shopInfo.lng == null || !patternCoords.test(shopInfo.lat) || !patternCoords.test(shopInfo.lng)) {
+        return;
+    }
+
+    var googleMapsLatLng = new google.maps.LatLng(shopInfo.lat, shopInfo.lng);
+
+    var marker = new google.maps.Marker({
+        position: googleMapsLatLng,
+        map: map,
+        title: shopInfo.title,
+        draggable: isDraggable
+    });
+
+    bounds.extend(googleMapsLatLng);
+
+    map.setCenter(googleMapsLatLng);
+    marker.setMap(map);
+    markers.push(marker);
+
+    var infowindow = new google.maps.InfoWindow({
+        maxWidth: 400,
+        content: '<h5>' + shopInfo.title + '</h5>' + '<div>' + $('.shops-list > li:eq(' + shopInfo.index + ') .short-description').html() + '</div>'
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.open(map, marker);
+        $('.shops-list > li').removeClass('active');
+        $('.shops-list > li[data-ind="' + shopInfo.index + '"]').addClass('active');
+    });
+
+    //return markers.length-1;
+}
+
+// Sets the map on all markers in the array.
+function setAllMap(mapa) {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(mapa);
+    }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+    setAllMap(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+    setAllMap(map);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+    clearMarkers();
+    markers = [];
+}
+
+function createMapSearchBox() {
+    var input = document.getElementById("shop-address-input");
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    var searchBox = new google.maps.places.SearchBox(input);
+    input.style.display = "block";
+
+    google.maps.event.addListener(searchBox, 'places_changed', function () {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        createUserMarker(places[0].geometry.location);
+        userMarker.setPosition(places[0].geometry.location);
+        map.setCenter(places[0].geometry.location);
+
+        searchResultCoords = places[0].geometry.location;
+
+        showDistancesFromShopsToUser();
+    });
+}
+
+function createUserMarker(location) {
+    if (!isUserMarkerCreated) {
+        var image = $('.shop-resources').attr('data-pathtoimages') + '/squat_marker_green_thumb.png';
+
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map,
+            icon: image,
+            animation: google.maps.Animation.DROP,
+            title: $('.shop-resources').attr('data-youarehere'),
+            draggable: true
+        });
+
+        marker.setMap(map);
+        userMarker = marker;
+
+        bounds.extend(location);
+
+        $('.distanceAndDirections').show();
+        $('#sortingSelect option[value="sortbydistance"]').removeAttr('disabled');
+        
+        isUserMarkerCreated = true;
+
+        google.maps.event.addListener(marker, 'dragend', function () {
+            showMarkers();
+            $('.shops-list > li').show();
+            showDistancesFromShopsToUser();
+        });
+    }
+}
+
+function getUserGeoLocation() {
+    var message = 'Geolocation is not supported by this browser.';
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            message = 'Now we use your current position. ( Maybe a confirmation is required by the browser to use it. )';
+            //message = "Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude;
+
+            var userCoords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+            createUserMarker(userCoords);
+            userMarker.setPosition(userCoords);
+            map.setCenter(userCoords);
+
+            showDistancesFromShopsToUser();
+        });
+    }
+
+    return message;
+}
+
+function showDirectionsToShop(ind) {
+    if (markers[ind] != null) {
+        var areUnitsImperial = $('.shop-resources').attr('data-units') == 'Imperial';
+
+        var request = {
+            origin: new google.maps.LatLng(userMarker.position.lat(), userMarker.position.lng()),
+            destination: new google.maps.LatLng(markers[ind].position.lat(), markers[ind].position.lng()),
+            travelMode: google.maps.TravelMode.DRIVING,
+            unitSystem: areUnitsImperial ? google.maps.UnitSystem.IMPERIAL : google.maps.UnitSystem.METRIC
+        };
+
+        directionsService.route(request, function (response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setMap(map);
+                directionsDisplay.setDirections(response);
+            }
+            $('#directions-panel, .map-wrapper, #backToResults').addClass('directions-shown');
+        });
+    }
+}
+
+function showDistancesFromShopsToUser() {
+    $('.shops-list > li').each(function () {
+        var shop = $(this);
+        var ind = shop.attr('data-ind');
+
+        if (markers[ind] != null) {
+            getDistanceToPosition(markers[ind].position, shop);
+        }
+    });
+
+    $('#sortingSelect option[value="sortbydistance"]').attr('selected', 'selected');
+    sortShopsByDistance();
+}
+
+// Converts from degrees to radians.
+Math.toRadians = function (degrees) {
+    return degrees * Math.PI / 180;
+};
+
+function getDistanceToPosition(destination, shop) {
+
+    // Code taken from http://www.movable-type.co.uk/scripts/latlong.html
+
+    // Google says:
+    // 1 kilometer = 0.621371192 miles
+    // 1 mile = 1.609344 kilometers
+    var areUnitsImperial = $('.shop-resources').attr('data-units') == 'Imperial';
+
+    var radius = 6371e3;
+    var lat1 = Math.toRadians(userMarker.position.lat());
+    var lng1 = Math.toRadians(userMarker.position.lng());
+    var lat2 = Math.toRadians(destination.lat());
+    var lng2 = Math.toRadians(destination.lng());
+    var deltaLat = lat2 - lat1;
+    var delatLng = lng2 - lng1;
+
+    var a = Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2) + Math.cos(lat1) * Math.cos(lat2) * Math.sin(delatLng / 2) * Math.sin(delatLng / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = radius * c / 1000;
+
+    if (areUnitsImperial) {
+        shop.find('.distance-value').html(Math.round((d * 0.621371192)) + ' mi.');
+        shop.find('.distanceToShopValue').val((d * 0.621371192));
+    } else {
+        shop.find('.distance-value').html(Math.round(d) + ' km.');
+        shop.find('.distanceToShopValue').val(d);
+    }
+}
+
+// Sorting and Filtering
+
+function sortShopsBySelectedMethod() {
+    var sortMethod = $('#sortingSelect').val();
+
+    if (sortMethod == 'sortbydistance') {
+        sortShopsByDistance();
+    } else if (sortMethod == 'sortbyname') {
+        sortShopsByName();
+    } else if (sortMethod == 'sortbyid') {
+        sortShopsById();
+    }
+}
+
+function sortShopsByName() {
+    var shops = $('.shops-list > li');
+    shops.sort(function (a, b) {
+        var aName = $(a).find('.shop-name').html();
+        var bName = $(b).find('.shop-name').html();
+
+        return aName.localeCompare(bName);
+    });
+
+    $('.shops-list').html(shops);
+}
+
+function sortShopsByDistance() {
+    if (!isUserMarkerCreated) {
+        return;
+    }
+
+    var shops = $('.shops-list > li');
+    shops.sort(function (a, b) {
+        var aDist = parseFloat($(a).find('.distanceToShopValue').val());
+        var bDist = parseFloat($(b).find('.distanceToShopValue').val());
+
+        return aDist - bDist;
+    });
+
+    $('.shops-list').html(shops);
+}
+
+function sortShopsById() {
+    if (!isUserMarkerCreated) {
+        return;
+    }
+
+    var shops = $('.shops-list > li');
+    shops.sort(function (a, b) {
+        var aId = parseInt($(a).attr('data-ind'));
+        var bId = parseInt($(b).attr('data-ind'));
+
+        return aId - bId;
+    });
+
+    $('.shops-list').html(shops);
+}
+
+function searchForShopsMatchingTheSearchCriteria() {
+    var shouldSearchByDistanceCriteria = isUserMarkerCreated;
+    var searchedTag = $('#searchByTagsInput').val();
+    var shouldSearchByTagsCriteria = searchedTag.length > 2;
+
+    var radius = parseFloat($('#searchRadius').val());
+    if (isNaN(radius)) {
+        shouldSearchByDistanceCriteria = false;
+    }
+
+    if (!shouldSearchByDistanceCriteria && !shouldSearchByTagsCriteria) {
+        return;
+    }
+
+    $('.shops-list > li').each(function () {
+        var shop = $(this);
+        var ind = parseInt(shop.attr('data-ind'));
+
+        var doesDistanceCriteriaMatch = true;
+        var doesTagsCriteriaMatch = true;
+
+        if (shouldSearchByDistanceCriteria) {
+            var distanceToShop = parseFloat(shop.find('.distanceToShopValue').val());
+            if (distanceToShop > radius) {
+                doesDistanceCriteriaMatch = false;
+            }
+        }
+
+        if (shouldSearchByTagsCriteria) {
+            var shopTags = shop.find('.shop-tags-hidden-field').val();
+
+            if (shopTags == undefined || shopTags.indexOf(searchedTag) == -1) {
+                doesTagsCriteriaMatch = false;
+            }
+        }
+
+        if (doesDistanceCriteriaMatch && doesTagsCriteriaMatch) {
+            shop.show();
+            if (markers[ind] != null) {
+                markers[ind].setMap(map);
+            }
+        } else {
+            shop.hide();
+            if (markers[ind] != null) {
+                markers[ind].setMap(null);
+            }
+        }
+    });
+
+    if ($('.shops-list > li:visible').length == 0) {
+        $('.no-shops-after-filtering').show();
+    }
+    else {
+        $('.no-shops-after-filtering').hide();
+    }
+
+    sortShopsBySelectedMethod();
+}
---
+++ Presentation/Nop.Web/Themes/Urban/Content/scripts/Urban.js
@@ -0,0 +1,286 @@
+﻿
+
+(function () {
+    $(document).ready(function () {
+
+        // add loading effect 
+
+        //window.addEventListener('DOMContentLoaded', function () {
+        //    "use strict";
+        //    var ql = new QueryLoader2(document.querySelector("body"), {
+        //        barColor: "#efefef",
+        //        backgroundColor: "#111",
+        //        percentage: true,
+        //        barHeight: 1,
+        //        minimumTime: 200,
+        //        fadeOutTime: 1000,
+        //        onProgress: true
+        //    });
+        //});
+        
+
+        var responsiveMenuSettings = {
+            doesBackgroundChange: false,
+            doesPaddingChange: false,
+            bgSelector: ".mega-menu-responsive > li > ul, .top-menu > li > .sublist",
+            bgInitialColor: $(".menu-title span").css("background-color"),
+            red: 40,
+            green: 40,
+            blue: 40,
+            alpha: 0.05,
+            paddingSelector: ".header-menu > ul > li",
+            paddingValue: 0,
+            themeBreakpoint: 1000
+        };
+
+        var responsiveAppSettings = {
+            isEnabled: true,
+            isSearchBoxDetachable: false,
+            isHeaderLinksWrapperDetachable: false,
+            doesDesktopHeaderMenuStick: false,
+            doesScrollAfterFiltration: true,
+            doesSublistHasIndent: true,
+            displayGoToTop: true,
+            hasStickyNav: false,
+            selectors: {
+                menuTitle: ".header-menu-opener",
+                headerMenu: ".header-menu",
+                closeMenu: ".close-menu",
+                movedElements: ".admin-header-links, .header, .responsive-nav-wrapper, .master-wrapper-content, .footer",
+                sublist: ".header-menu .sublist",
+                overlayOffCanvas: ".overlayOffCanvas",
+                withSubcategories: ".with-subcategories",
+                filtersContainer: ".nopAjaxFilters7Spikes",
+                filtersOpener: ".filters-button span",
+                searchBoxOpener: ".search-wrap > span",
+                searchBox: ".search-box",
+                searchBoxBefore: ".header-logo",
+                navWrapper: ".responsive-nav-wrapper",
+                navWrapperParent: ".responsive-nav-wrapper-parent",
+                headerLinksOpener: "#header-links-opener",
+                headerLinksWrapper: ".header-links-wrapper",
+                shoppingCartLink: "",
+                overlayEffectDelay: 300
+            }
+        };
+
+        initResponsiveTheme(responsiveMenuSettings, responsiveAppSettings);
+
+        // Header links
+        $('.shopping-cart-opener a.ico-cart').on('click', function(e) { e.preventDefault(); });
+
+        headerLinksClickable('.shopping-cart-opener', '.header-links-wrapper .shopping-cart-wrapper');
+        headerLinksClickable('.user-links-opener', '.header-links-wrapper .user-links-wrapper');
+        headerLinksClickable('.search-opener', '.header-links-wrapper .search-box-wrapper');
+        headerLinksClickable('.header-selectors-opener', '.header-links-wrapper .header-selectors-wrapper');
+
+        // Set white background to header strip in mobile layout
+        $('.home-page.header-inside-slider .header.mobile .header-strip li').on('click', function () {
+            if ($('.home-page.header-inside-slider .header.mobile .header-strip li.active').length > 0) {
+                $('.header-strip').addClass('white-background');
+            } else {
+                $('.header-strip').removeClass('white-background');
+            }
+        });
+
+        // Numeric only 
+        $('.add-to-cart-panel .qty-input, .productQuantityTextBox').ForceNumericOnly();
+
+        // Call select customisation function
+        if ($.getSpikesViewPort().width >= responsiveMenuSettings.themeBreakpoint) {
+            customSelect();
+        }
+
+        // Custom scroll
+        $('.header.mobile .fullWidth, .header.mobile .header-menu, .header.mobile .mega-menu-responsive ').perfectScrollbar({
+            swipePropagation: false,
+            wheelSpeed: 1,
+            suppressScrollX: true
+        });
+
+        // Make social icons 2 in line if even
+        var socIcons = $('.social-buttons li');
+        if (socIcons.length % 2 === 0 && socIcons.length < 5) {
+            socIcons.addClass('reshuffle');
+        }
+
+        // Toggle filters
+        $(".horizontalFiltersTitle").click(function () {
+            $(this).toggleClass('opened');
+            $(".onecolumn .filtersPanel").slideToggle("slow", function () {
+                $('.onecolumn .filtersPanel').css({ 'padding': '0' });
+                $(this).css('overflow', '');
+            });
+        });
+
+        // Case when category banner is only one
+        if ($('.banner-included .slider-wrapper').length == 1) {
+            $('.home-page-category-grid.banner-included .item-box:nth-child(n+4)').addClass('oneBannerOnly');
+            $('.home-page-category-grid.banner-included .slider-wrapper').parents('.item-grid').addClass('oneBannerIncluded');
+            $('.oneBannerOnly').wrapAll('<div class="clearingWrapper"></div>');
+        }
+
+        // Plus, Minus Quantity buttons
+        // Normal & Ajax Cart
+        $('.item-box').on('click', '.plus', incrementQuantityValue);
+        $('.item-box').on('click', '.minus', decrementQuantityValue);
+        $('.ajaxCart').on('click', '.plus', incrementQuantityValue);
+        $('.ajaxCart').on('click', '.minus', decrementQuantityValue);
+        $('.variant-overview').on('click', '.plus', incrementQuantityValue);
+        $('.variant-overview').on('click', '.minus', decrementQuantityValue);
+        // Home Page Categories Tabs Normal & Ajax Cart
+        $('.home-page-category-tabs').on('click', '.plus', incrementQuantityValue);
+        $('.home-page-category-tabs').on('click', '.minus', decrementQuantityValue);
+        // Quick View
+        $('.quickView').on('click', '.plus', incrementQuantityValue);
+        $('.quickView').on('click', '.minus', decrementQuantityValue);
+        // product Details Page
+        $('.product-essential .plus').on('click', incrementQuantityValue);
+        $('.product-essential .minus').on('click', decrementQuantityValue);
+
+        // Equal height elements
+        equalizeElementsHeights(responsiveMenuSettings.themeBreakpoint);
+        $.addSpikesWindowEvent("resize", function () { equalizeElementsHeights(responsiveMenuSettings.themeBreakpoint); });
+        $.addSpikesWindowEvent("orientationchange", function () { equalizeElementsHeights(responsiveMenuSettings.themeBreakpoint); });
+        
+        // Home Page Slider Position
+        homePageSliderUnderHeader();
+
+        // Close header links on click outside 
+        $('html').click(function () {
+            $('.header-links-wrapper,.header-links-wrapper>div ').removeClass('active');
+            $('.header-inside-slider .header-strip').removeClass('white-background');
+        });
+
+        $('.header-strip').click(function (event) {
+            event.stopPropagation();
+        });
+
+
+        //footer footer-box
+        if ($('.footer-box').length == 2) {
+            $('.footer-box').css('width', '50%');
+        }
+        
+
+    });
+    
+        function equalizeElementsHeights(themeBreakpoint) {
+            if ($.getSpikesViewPort().width >= themeBreakpoint) {
+                //equalHeight($('.footer-box, .address-item'));
+                //equalHeight($('.twocolumns>div, .order-review-data>ul'));
+                equalHeight($('.sitemap-page .entity'));
+                //equalHeight($(' .center-2, .side-2'));
+                equalHeight($('.footer-box'));
+            }
+        }
+    
+
+    function incrementQuantityValue(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        var input = $(this).siblings('.qty-input, .productQuantityTextBox').first();
+
+        var value = parseInt(input.val());
+        if (isNaN(value)) {
+            input.val(1);
+            return;
+        }
+
+        value++;
+        input.val(value);
+    }
+
+    function decrementQuantityValue(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        var input = $(this).siblings('.qty-input, .productQuantityTextBox').first();
+
+        var value = parseInt(input.val());
+
+        if (isNaN(value)) {
+            input.val(1);
+            return;
+        }
+
+        if (value <= 1) {
+            return;
+        }
+
+        value--;
+        input.val(value);
+    }
+
+    function headerLinksClickable(anchor, target) {
+        $(anchor).on('click', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            if ($(target).hasClass('active')) {
+                $(target + ', ' + anchor).removeClass('active');
+            } else {
+                $(target + ', ' + anchor).siblings().removeClass('active');
+                $(target + ', ' + anchor + ', .header-links-wrapper').addClass('active');
+            }
+        });
+    }
+
+    function equalHeight(group) {
+        var tallest = 0;
+        group.each(function () {
+            var thisHeight = $(this).height();
+            if (thisHeight > tallest) {
+                tallest = thisHeight;
+            }
+        });
+        group.height(tallest);
+    }
+
+    jQuery.fn.ForceNumericOnly = function () {
+        return this.each(function () {
+            $(this).keydown(function (e) {
+                var key = e.charCode || e.keyCode || 0;
+                // allow backspace, tab, delete, enter, arrows, numbers and keypad numbers ONLY
+                // home, end, period, and numpad decimal
+                return (
+                    key == 8 ||
+                    key == 9 ||
+                    key == 13 ||
+                    key == 46 ||
+                    key == 110 ||
+                    key == 190 ||
+                    (key >= 35 && key <= 40) ||
+                    (key >= 48 && key <= 57) ||
+                    (key >= 96 && key <= 105));
+            });
+        });
+
+        
+    };
+
+    //custom select elements
+    function customSelect() {
+        $('.header-selectors-wrapper select, .product-selectors select, .urban-theme-roller select').each(function () {
+            var that = $(this);
+
+            that.wrap('<div class="custom-select" />');
+            $('<div class="custom-select-text" />').prependTo(that.parent('.custom-select'));
+            that.siblings('.custom-select-text').text(that.children('option:selected').text());
+        }).change(function () {
+            $(this).siblings('.custom-select-text').text($(this).children('option:selected').text());
+        });
+    }
+
+    
+
+    //homePageSlider
+    function homePageSliderUnderHeader(){
+        if ($('.home_page_main_slider').length) {
+            $('.master-wrapper-page.home-page.header-inside-slider .header').addClass('becomeAbsolute');
+        }
+    }
+    
+
+})();
